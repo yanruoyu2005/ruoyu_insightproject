624e8d347d387b1d57fdf37072df94896fc0d62f
ruoyu <yanruoyu2005@gmail.com>
2020-06-05 15:14:14 -0400

    flask demo for minimum viable product

---
+++ projectname/projectname/just_test.py
@@ -0,0 +1,71 @@
+import os
+import sys
+
+from keras.layers import Dense
+from keras import Sequential
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.metrics import confusion_matrix
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import load_model
+import itertools
+
+def get_script_path():
+    return os.path.dirname(os.path.realpath(sys.argv[0]))
+
+def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    pic_name = path1 + '\static\confusion_matrix.png'
+    plt.savefig(pic_name)
+    
+    
+def test_single(number):
+    global path1
+    path1 = get_script_path()
+    path_data = path1 + '\exploratory_data'
+    classifier=load_model(path_data + '\pretrain_model.hdf5')
+    X_test_pd = pd.read_csv(path_data + '\X_test_pd.csv')
+    X_test = np.asarray(X_test_pd.iloc[:,1:])
+    y_test_pd = pd.read_csv(path_data + '\y_test_pd.csv')
+    y_test = np.asarray(y_test_pd.iloc[:,1:])
+
+    y_pred=classifier.predict(X_test)
+    y_pred1 =(y_pred>0.5)
+    cm = confusion_matrix(y_test, y_pred1)
+    if y_pred[number] > 0.5:
+        prediction = 'Success'
+    if y_pred[number] < 0.5:
+        prediction = 'Failure'
+    if y_test[number] == 1:
+        actual = 'Success'
+    if y_test[number]==0:
+        actual = 'Failure'   
+    print ('Predicted outcome, ', prediction)
+    print ('Actual outcome, ', actual)
+    cm_plot_labels = ['Faliure','Success']
+    plot_confusion_matrix(cm=cm, classes=cm_plot_labels, title='Confusion Matrix')
+    return prediction, actual
---
+++ projectname/projectname/server.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request
+from just_test import test_single 
+
+
+
+# Create the application object
+app = Flask(__name__)
+
+@app.route('/',methods=["GET","POST"]) #we are now using these methods to get user input
+def home_page():
+    return render_template('index.html')  # render a template
+
+@app.route('/output')
+def recommendation_output():
+#       
+
+       # Pull input
+       some_input =request.args.get('user_input')            
+       
+       # Case if empty
+       if some_input =="":
+           return render_template("index.html",
+                                  my_input = some_input,
+                                  my_form_result="Empty")
+       else:
+           some_output="yeay!"
+           number = int(some_input)
+           prediction,actual = test_single(number)
+           some_number = 'Prediction: ' + prediction + '  Actual: ' + actual
+           #print ('Predicted outcome, ', prediction)
+           #print ('Actual outcome, ', actual)
+           #some_image="giphy.gif"
+           some_image="confusion_matrix.png"
+           return render_template("index.html",
+                              my_input=some_input,
+                              my_output=some_output,
+                              my_number=some_number,
+                              my_img_name=some_image,
+                              my_form_result="NotEmpty")
+
+
+# start the server with the 'run()' method
+if __name__ == "__main__":
+    app.run(threaded=False, debug=True) #will run locally http://127.0.0.1:5000/
+
+
